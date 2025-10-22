5fb249f2bbd5db6748c0acd5ea2acfef1a967bce
TiagoAtahualpa <tiago.atahualpa@gmail.com>
2025-10-22 12:10:34 -0300

    Atualizações da página(geral)

--- script.js
+++ script.js
@@ -41,3 +41,51 @@ document.addEventListener('click', (event) => {
         closeMainMenu();
     }
 });
+
+
+
+
+
+//SWIPER
+
+const clientsSwiper = new Swiper('.clients-swiper', {
+    // 1. Configuração para carrossel de logos
+    direction: 'horizontal', // Deve ser horizontal
+    loop: true,             // Rolagem infinita
+    grabCursor: true,     //mãozinha
+    // 2. Quantidade de slides visíveis - CRUCIAL para mostrar 5 logos
+    slidesPerView: 5, 
+    spaceBetween: 30,       // Espaçamento entre as logos
+    
+    // 3. Autoplay para rolar automaticamente
+    autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+    },
+
+    // 4. Configuração de responsividade
+    breakpoints: {
+        768: { slidesPerView: 5 },
+        480: { slidesPerView: 3 },
+        0: { slidesPerView: 2 }
+    },
+  
+});
+
+
+// SWIPER PARA O HERO SECTION IMG-FUNDO ( INDEX (INICIO) )
+const heroSwiper = new Swiper('.hero-bg-swiper', { 
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 1, 
+    effect: 'fade', // Efeito suave
+    
+    // 
+    autoplay: {
+        delay: 3000, //3 segundos
+        disableOnInteraction: false,
+    },
+});
+
+
+
